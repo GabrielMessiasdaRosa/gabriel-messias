c3ce412389f6c421bc8d7c8810ea78f797cb0c80
Gabriel Messias <gabrielmessiasdarosa@gmail.com>
2023-03-14 18:23:29 -0300

    refact: improvements to skills marquee

--- src/screens/home-screen/skills-marquee.tsx
+++ src/screens/home-screen/skills-marquee.tsx
@@ -4,38 +4,28 @@ import Row from "../../components/row";
 export type SkillsMarqueeProps = {};
 
 const SkillsMarquee = ({}: SkillsMarqueeProps) => {
+  const skills = [
+    "SEO",
+    "Front-end Development",
+    "API consumption",
+    "Best practices",
+    "Performance",
+    "Accessibility",
+    "Clean code",
+  ];
   return (
     <Col className="h-24 w-full justify-center bg-gradient-to-tr from-[#ee0979] to-[#ff6a00]">
       <Row className="-ml-20 h-2/3 min-w-[2000px] -rotate-[2deg] items-center justify-center border bg-gray-50 ">
         <Marquee gradient speed={100}>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>SEO</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>Front-end Development</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>API consumption</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>Best practices</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>Performance</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>Accessibility</span>
-          </Row>
-          <Row className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black">
-            <span className="text-4xl">&#10024;</span>
-            <span>Clean code</span>
-          </Row>
+          {skills.map((skill, index) => (
+            <Row
+              key={index}
+              className="m-auto items-center justify-center space-x-5  text-3xl font-bold text-black"
+            >
+              <span className="text-4xl">&#10024;</span>
+              <span>{skill}</span>
+            </Row>
+          ))}
         </Marquee>
       </Row>
     </Col>
