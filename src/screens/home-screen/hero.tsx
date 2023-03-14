5e455fce85f7482dfa3d12bd0004b882449aa1af
Gabriel Messias <gabrielmessiasdarosa@gmail.com>
2023-03-14 18:23:50 -0300

    refact: some minor fixes

--- src/screens/home-screen/hero.tsx
+++ src/screens/home-screen/hero.tsx
@@ -10,20 +10,20 @@ const Hero = ({}: HeroProps) => {
           src="https://media.licdn.com/dms/image/C4D03AQEpHyv9RwJeVA/profile-displayphoto-shrink_800_800/0/1619282232881?e=1684368000&v=beta&t=Y16aHfqnbv3DjX1KwN0MaA7vg4CtWEk05iTdIgelfmw"
           width={220}
           height={220}
-          className="overflow-hidden rounded-full"
+          className="-z-10 overflow-hidden rounded-full"
         />
         <Col className="items-center">
           <Col className="text-center">
             <h1>
-              <span className="bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-3xl md:text-5xl font-extrabold text-transparent">
+              <span className="bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-3xl font-extrabold text-transparent md:text-5xl">
                 Hi, im Gabriel,{" "}
               </span>
-              <span className="text-2xl md:text-4xl font-extrabold">
+              <span className="text-2xl font-extrabold md:text-4xl">
                 front-end developer from Brazil.
               </span>
             </h1>
           </Col>
-          <h2 className="w-3/4 text-center text-base md:text-lg font-extrabold text-gray-400 md:w-1/2">
+          <h2 className="w-3/4 text-center text-base font-extrabold text-gray-400 md:w-1/2 md:text-lg">
             I'm a passionate developer who loves to create things that live on
             the internet, my main goal is to always build products that provide
             pixel-perfect, performant experiences.
