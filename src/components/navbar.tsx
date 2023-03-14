08fb39d5ac325821ce6e74b5ead1013c5be63598
Gabriel Messias <gabrielmessiasdarosa@gmail.com>
2023-03-14 18:24:59 -0300

    feat: improvements to navbar, headless ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
-import { Disclosure } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
 import Col from "./col";
 import Container from "./container";
 import Row from "./row";
@@ -17,68 +18,94 @@ function classNames(...classes) {
 const Navbar = ({}: NavbarProps) => {
   return (
     <Container>
-      <Disclosure as="nav" className="w-full bg-transparent">
-        {({ open }) => (
-          <>
-            <Row className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-              <Row className="relative h-16 w-screen items-center justify-between">
-                <Row className="absolute inset-y-0 left-2 items-center sm:hidden">
-                  {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </Row>
-                <Row className="w-full items-center justify-center px-4 sm:items-stretch sm:justify-between">
-                  <Col>
-                    <p className="ml-3 bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-2xl font-extrabold text-transparent md:ml-0">
-                      Gabriel Messias da Rosa
-                    </p>
-                  </Col>
-                  <Col className="hidden sm:ml-6 sm:block">
-                    <Row className="space-x-4">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? "text-orange-500 transition-all duration-300 hover:text-orange-600"
-                              : "text-gray-300 transition-all duration-300 hover:text-orange-600",
-                            "rounded-md px-3 py-2 text-sm font-medium"
-                          )}
-                          aria-current={item.current ? "page" : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </Row>
-                  </Col>
-                </Row>
-              </Row>
-            </Row>
-
-            <Disclosure.Panel className="sm:hidden">
-              <Col className="items-center space-y-1">
+      <Row className="relative max-w-7xl px-2 sm:px-6 lg:px-8">
+        <Row className="relative h-16 w-screen items-center justify-between">
+          <Row className="absolute inset-y-0 right-6 top-1 items-center justify-center sm:hidden">
+            {/* Mobile menu button*/}
+            <div className="sm:hidden ">
+              <Menu as="div" className="relative">
+                {({ open }) => {
+                  return (
+                    <>
+                      <Menu.Button>
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XMarkIcon
+                            className="block h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <Bars3Icon
+                            className="block h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        )}
+                      </Menu.Button>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 w-[15rem] origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Col className="items-center space-y-2 rounded-sm py-4">
+                            {navigation.map((navItem, index) => (
+                              <Menu.Item key={index} as={Fragment}>
+                                {({ active }) => {
+                                  return (
+                                    <a
+                                      href={navItem.href}
+                                      className="font-thin text-black"
+                                    >
+                                      {navItem.name}
+                                    </a>
+                                  );
+                                }}
+                              </Menu.Item>
+                            ))}
+                          </Col>
+                        </Menu.Items>
+                      </Transition>
+                    </>
+                  );
+                }}
+              </Menu>
+            </div>
+          </Row>
+          <Row className="w-full items-center justify-center px-4 sm:items-stretch sm:justify-between">
+            <Col>
+              <p className="hidden bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-2xl font-extrabold text-transparent sm:flex md:ml-0">
+                Gabriel Messias da Rosa
+              </p>
+              <p className="bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-2xl font-extrabold text-transparent sm:hidden md:ml-0">
+                GMR
+              </p>
+            </Col>
+            <Col className="hidden sm:ml-6 sm:block">
+              <Row className="space-x-4">
                 {navigation.map((item) => (
-                  <Disclosure.Button
+                  <a
                     key={item.name}
-                    as="a"
                     href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "text-orange-500 transition-all duration-300 hover:text-orange-600"
+                        : "text-gray-300 transition-all duration-300 hover:text-orange-600",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
                     aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
-                  </Disclosure.Button>
+                  </a>
                 ))}
-              </Col>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+              </Row>
+            </Col>
+          </Row>
+        </Row>
+      </Row>
     </Container>
   );
 };
