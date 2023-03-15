511509d303c950b64380f95905c53a643fd2e144
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-03-15 07:13:03 -0300

    refact: minor fixes

--- src/screens/home-screen/selected-projects.tsx
+++ src/screens/home-screen/selected-projects.tsx
@@ -65,8 +65,8 @@ const SelectedProductsSection = ({}: SelectedProjectsSectionProps) => {
           </Col>
         </Col>
         <Col className="space-y-8">
-          {projects.map((project) => (
-            <Col className="group relative overflow-hidden">
+          {projects.map((project, index) => (
+            <Col key={index} className="group relative overflow-hidden">
               <Image
                 src={project.image}
                 alt="Selected Projects"
@@ -74,24 +74,26 @@ const SelectedProductsSection = ({}: SelectedProjectsSectionProps) => {
                 height={800}
                 className="rounded-xl"
               />
-              <Col className="bottom-0 w-full bg-gradient-to-r from-[#161513] via-[#161513]/90 to-[#161513]/50 transition-all duration-200 ease-linear lg:absolute lg:-mb-72 lg:group-hover:-mb-0 ">
+              <Col className="bottom-0 w-full bg-gradient-to-r from-[#161513] via-[#161513]/90 to-[#161513]/50 transition-all duration-500 ease-in-out lg:absolute lg:-mb-96 lg:group-hover:-mb-0 ">
                 <Col className="space-y-2 py-6 lg:p-20">
                   <p className="text-2xl font-thin lg:text-5xl">
                     {project.name}
                   </p>
                   <p className="text-xl font-thin lg:text-2xl">Tech Stack:</p>
-                  <Col className="space-y-2 md:flex-row md:space-y-0 md:space-x-2">
-                    {project.techStack.map((tech) => (
-                      <>
-                        <Row className="w-1/2 items-center justify-center whitespace-nowrap rounded-full bg-white bg-gradient-to-br from-[#ee0979] to-[#ff6a00] p-[1px] font-light lg:hidden">
-                          <p className="text-lg lg:hidden">{tech}</p>
-                        </Row>
-                        <Row className="hidden items-center justify-center whitespace-nowrap rounded-full bg-white bg-gradient-to-br from-[#ee0979] to-[#ff6a00] p-[1px] text-lg font-light  lg:flex">
-                          <p className="rounded-full bg-[#161513] px-4">
+                  <Col className="grid md:w-1/2 grid-cols-2 gap-2 md:grid-cols-2">
+                    {project.techStack.map((tech, index) => (
+                      <div key={index}>
+                        <Row className="w-full items-center justify-center whitespace-nowrap rounded-full bg-white bg-gradient-to-br from-[#ee0979] to-[#ff6a00] p-[1px] font-light lg:hidden">
+                          <p className="flex w-full items-center justify-center rounded-full bg-[#161513] px-2 text-lg lg:hidden">
                             {tech}
                           </p>
                         </Row>
-                      </>
+                        <Row className="hidden w-full items-center justify-center whitespace-nowrap rounded-full bg-white bg-gradient-to-br from-[#ee0979] to-[#ff6a00] p-[1px] text-lg font-light  lg:flex">
+                          <p className="flex w-full items-center justify-center rounded-full bg-[#161513] px-4">
+                            {tech}
+                          </p>
+                        </Row>
+                      </div>
                     ))}
                   </Col>
                 </Col>
