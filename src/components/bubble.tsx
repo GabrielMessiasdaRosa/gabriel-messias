e2f4c77c409e261b94f3206051baf1480bb8520b
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-03-29 19:39:17 -0300

    fix: fix

--- src/components/bubble.tsx
+++ src/components/bubble.tsx
@@ -21,18 +21,18 @@ const Bubble = ({}: BubbleProps) => {
           x: animateX,
           y: animateY,
         }}
-        className={`absolute -z-50 h-96 w-96 bg-gradient-to-br from-[#ee0979] to-[#ff6a00] opacity-75 transition-all ease-linear  ${
+        className={`absolute -z-50 h-96 w-96  opacity-75 transition-all ease-linear  ${
           isInHeroSection
-            ? "shape-1"
+            ? "shape-1 bg-gradient-to-br from-[#ee0979] to-[#ff6a00]"
             : isInProjectsSection
-            ? "shape-2"
+            ? "shape-2 bg-gradient-to-br from-[#ee0979] to-[#ff6a00]"
             : isInProjetc1Section
             ? "shape-3 bg-gradient-to-r from-[#ffc400] to-[#00c9c9]"
             : isInProjetc2Section
             ? "shape-4 bg-gradient-to-b from-[#2a02bb] to-[#ffc400]"
             : isInProjetc3Section
             ? "shape-5 bg-gradient-to-r from-[#2a02bb] to-[#00c9c9]"
-            : "shape-6"
+            : "shape-6 bg-gradient-to-br from-[#ee0979] to-[#ff6a00]"
         }`}
       />
     </>
